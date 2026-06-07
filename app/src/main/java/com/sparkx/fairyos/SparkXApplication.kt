4724a6c737e15bafc55cf1cd0d6b588a794948df
iansparkman8 <iansparkman8@gmail.com>
2026-06-07 06:24:46 -0400

    Add SparkPersonalityRepository to SparkXApplication singleton

--- app/src/main/java/com/sparkx/fairyos/SparkXApplication.kt
+++ app/src/main/java/com/sparkx/fairyos/SparkXApplication.kt
@@ -2,15 +2,19 @@ package com.sparkx.fairyos
 
 import android.app.Application
 import com.sparkx.fairyos.domain.memory.TeachGrowRepository
+import com.sparkx.fairyos.domain.personality.SparkPersonalityRepository
 
 class SparkXApplication : Application() {
 
     lateinit var teachGrowRepository: TeachGrowRepository
         private set
 
+    lateinit var sparkPersonalityRepository: SparkPersonalityRepository
+        private set
+
     override fun onCreate() {
         super.onCreate()
         teachGrowRepository = TeachGrowRepository(this)
-        // Future: init other singletons
+        sparkPersonalityRepository = SparkPersonalityRepository(this)
     }
 }
