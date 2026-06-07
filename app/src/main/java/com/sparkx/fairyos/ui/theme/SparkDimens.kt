29352fdf63682c9819506f50e2f9f2e3a7668bbf
iansparkman8 <iansparkman8@gmail.com>
2026-06-07 07:02:40 -0400

    v14: Add SparkDimens design tokens

---
+++ app/src/main/java/com/sparkx/fairyos/ui/theme/SparkDimens.kt
@@ -0,0 +1,11 @@
+package com.sparkx.fairyos.ui.theme
+
+object SparkDimens {
+    val cornerRadius = 20.dp
+    val cardPadding = 16.dp
+    val sectionSpacing = 24.dp
+    val elementSpacing = 12.dp
+    val iconSize = 24.dp
+    val avatarSize = 260.dp
+    val commandBarHeight = 56.dp
+}
