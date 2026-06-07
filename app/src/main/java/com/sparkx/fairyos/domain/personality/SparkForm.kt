fa63c4bbb2c76233a17719fd41273e0d9cf6af6b
iansparkman8 <iansparkman8@gmail.com>
2026-06-07 06:24:24 -0400

    Add SparkForm enum for personality growth system

---
+++ app/src/main/java/com/sparkx/fairyos/domain/personality/SparkForm.kt
@@ -0,0 +1,10 @@
+package com.sparkx.fairyos.domain.personality
+
+enum class SparkForm {
+    DEFAULT_FAIRY,
+    CYBER_FAIRY,
+    STAR_FAIRY,
+    DREAM_FAIRY,
+    SHADOW_FAIRY,
+    ROYAL_FAIRY
+}
